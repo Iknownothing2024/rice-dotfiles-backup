c09d7275f49b5db522bd7970ffcfe4a50daf5faa
iknownothing2024 <minecraftchina123456@gmail.com>
2026-02-05 09:05:09 +0800

    greatly modifed

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- C 开发优化
+vim.opt.cindent = true
+vim.opt.cinoptions = ":0,l1,g0,N-s,E-s,t0,+2s,(2s,u2s"
+vim.opt.formatoptions = "croqnj"
